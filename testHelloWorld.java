38ebc7e21da568a77e22e9daf7109c9372706292
woodchuckLH <woodchuck@126.com>
2016-06-20 11:22:58 +0800

    add

---
+++ testHelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by woodchuck on 2016/6/16.
+ */
+public class testHelloWorld {
+    public static void main(String [] args){
+        HelloWorld hello = new HelloWorld();
+        hello.PrintHello();
+    }
+
+
+}
